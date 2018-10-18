2c6d18a695d4ad5b0dd6f007d1dc11e899d77f05
niant <antti.niemenpaa@gmail.com>
2018-10-18 12:35:17 +0300

    Make webcam have greyscale filtering

--- index.js
+++ index.js
@@ -5,43 +5,54 @@ const MEMORY_PAGE_BYTES = 1024 * 64;
  * Instantiates a WASM-module with its own memory
  * @param {string} path Path to the WASM-module
  */
-const loadWasmModule = async path => { 
-  const memory = new WebAssembly.Memory({initial: 256});
+const loadWasmModule = async path => {
+  const memory = new WebAssembly.Memory({ initial: 256 });
 
   const options = {
     env: {
       memory,
       // We won't be using a table in this example, so set its initial size to 0
-      table: new WebAssembly.Table({initial: 0, maximum: 10, element: 'anyfunc'}),
+      table: new WebAssembly.Table({
+        initial: 0,
+        maximum: 10,
+        element: "anyfunc"
+      }),
       // Rest of the options are boilerplate for emsdk
       memoryBase: 1024,
       tableBase: 0,
       STACKTOP: 0,
       STACK_MAX: memory.buffer.byteLength,
-      abortStackOverflow: () => {throw new Error('Stack overflow');}
+      abortStackOverflow: () => {
+        throw new Error("Stack overflow");
+      }
     }
-  }
+  };
 
   const wasm = await WebAssembly.instantiateStreaming(fetch(path), options);
 
   return {
     ...wasm.instance.exports,
     memory
-  }
+  };
+};
+
+const loadWasm = async () => {
+  return await loadWasmModule("./dist/filter.wasm");
 };
 
 /**
  * Applies a grayscale-filter to an image
- * @param {ImageData} imageData 
+ * @param {ImageData} imageData
  */
-const filterImage = async imageData => {
-  const {memory, _grayscale: grayscale} = await loadWasmModule('./dist/filter.wasm');
-  const {data: pixelData, height, width} = imageData;
+const filterImage = (memory, grayscale, imageData) => {
+  const { data: pixelData, height, width } = imageData;
 
   // Calculate how much memory we have available
   const currentMemoryAmount = memory.buffer.byteLength / MEMORY_PAGE_BYTES;
   // Calculate how much memory we need to for the pixel data of the image
-  const requiredMemoryAmount = Math.ceil(pixelData.byteLength / MEMORY_PAGE_BYTES);
+  const requiredMemoryAmount = Math.ceil(
+    pixelData.byteLength / MEMORY_PAGE_BYTES
+  );
 
   // If we don't have enough memory, grow its size
   if (currentMemoryAmount < requiredMemoryAmount) {
@@ -51,34 +62,45 @@ const filterImage = async imageData => {
   // Create a TypedArray and use it to set the pixel data to the WASM memory
   const moduleMemory = new Uint8ClampedArray(memory.buffer);
   moduleMemory.set(pixelData);
-  
+
   // With the pixel data in the WASM memory, we can now grayscale it
   grayscale(moduleMemory, width, height);
-  
+
   // Read the grayscaled image from the memory
   const filteredPixelData = moduleMemory.subarray(0, pixelData.byteLength);
- 
+
   return new ImageData(filteredPixelData, width, height);
 };
 
-const img = new Image();
-img.src = '/sample.jpg';
+const filterWebcam = (videoElement, canvasElement, context, wam) => {
+  const width = videoElement.videoWidth || 640;
+  const height = videoElement.videoHeight || 480;
 
-img.onload = async () => {
-  const canvas = document.querySelector('.js-canvas');
-  const canvas2 = document.querySelector('.js-canvas2');
-  const context = canvas.getContext('2d');
-  const context2 = canvas2.getContext('2d');
-  
-  canvas.setAttribute('width', img.width);
-  canvas.setAttribute('height', img.height);
-  canvas2.setAttribute('width', img.width);
-  canvas2.setAttribute('height', img.height);
+  canvasElement.setAttribute("width", width);
+  canvasElement.setAttribute("height", height);
 
-  context.drawImage(img, 0, 0);
+  context.drawImage(videoElement, 0, 0);
 
-  const imageData = context.getImageData(0, 0, img.width, img.height);
-  const filteredImageData = await filterImage(imageData)
+  const imageData = context.getImageData(0, 0, width, height);
+  const filteredImageData = filterImage(wam.memory, wam._grayscale, imageData);
 
-  context2.putImageData(filteredImageData, 0, 0);
+  context.putImageData(filteredImageData, 0, 0);
 };
+
+let wam;
+loadWasm().then(module => (wam = module));
+
+const videoElement = document.querySelector("video");
+
+videoElement.addEventListener("play", () => {
+  const canvasElement = document.querySelector("canvas");
+  const context = canvasElement.getContext("2d");
+
+  const draw = () => {
+    if (videoElement.paused) return false;
+    filterWebcam(videoElement, canvasElement, context, wam);
+    requestAnimationFrame(draw);
+  };
+
+  draw();
+});

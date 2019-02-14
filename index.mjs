7c0029fba5b6c897cbd9d08e5f6dab7979d714bc
Juhani Kumara <juhani.kumara@solita.fi>
2019-02-14 13:48:22 +0200

    Better function names

--- index.mjs
+++ index.mjs
@@ -1,32 +1,29 @@
 import { drawVideoToCanvas } from "./canvas.mjs";
 import { toggleWebcam } from "./webcam.mjs";
 
-const MEMORY_PAGE_BYTES = 1024 * 64;
-
-const loadWasmModule = async() => {
+const loadWasmModule = async () => {
   // Create memory and instantiate the filter.wasm here
 
   // Return the instantiated module and the memory
   return {};
 };
 
-export const filterImage = (memory, filter) => imageData => {
+export const getImageFilterFn = (memory, filter) => imageData => {
   // Filter the imageData and return a new ImageData object
   // https://developer.mozilla.org/en-US/docs/Web/API/ImageData
   return imageData;
 };
 
-
-const main = async() => {
+const main = async () => {
   const videoElement = document.createElement("video");
   const canvasElement = document.querySelector("canvas");
 
   const wam = await loadWasmModule();
-  const filter = filterImage(wam.memory, wam._grayscale);
+  const filter = getImageFilterFn(wam.memory, wam._grayscale);
   drawVideoToCanvas(videoElement, canvasElement, filter);
 
   // Expose function to be used in the view
-  window.toggleVideo = () => toggleWebcam(videoElement);;
+  window.toggleVideo = () => toggleWebcam(videoElement);
 };
 
 main();
